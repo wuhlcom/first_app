7f77317d5a031ca062f924693a2d2450e8aa2a9a
wuhlcom <378433855@qq.com>
2016-08-01 23:24:59 -0400

    welecom

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 FirstApp::Application.routes.draw do
+  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
-  # Example of regular route:
+   root 'welcome#index'
+  
+# Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
